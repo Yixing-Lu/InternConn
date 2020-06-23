98a3ac946f6b08f59529828770ad5441decf2151
Zhou Shen <shenzhou1996@163.com>
2020-06-22 21:58:48 -0700

    Change YelpCamp into InternConn

--- routes/index.js
+++ routes/index.js
@@ -22,7 +22,7 @@ router.post("/register",function(req,res){
 			return res.render("register", {"error":err.message})
 		}
 		passport.authenticate("local")(req,res,function(){
-			req.flash("success","Welcome to YelpCamp" + user.username)
+			req.flash("success","Welcome to InternConn" + user.username)
 			res.redirect("/campgrounds")
 		})
 	})
