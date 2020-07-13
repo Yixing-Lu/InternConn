32fedf2ac6c0535d4b012f7b7ff60f239b633ebb
Yixing-Lu <yixinglu97@gmail.com>
2020-07-13 12:50:07 -0700

    add Account

--- routes/index.js
+++ routes/index.js
@@ -29,7 +29,7 @@ router.get("/register",function(req,res){
 
 // handle sign up logic
 router.post("/register",function(req,res){
-	var newUser = new User({username: req.body.username})
+	var newUser = new User({username: req.body.username, email: req.body.username})
 	User.register(newUser, req.body.password,function(err,user){
 		if(err) {
 			return res.render("register", {"error":err.message})
